9700e608310ef5705ab8a55f18f57998dca6a6cb
ponlawat-w <ponlawat_w@cmu.ac.th>
2021-05-25 21:46:48 +0700

    Remap Keys

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const VERSION = '2564042100';
+const VERSION = '2564042502';
 
 new Vue({
   el: '#app',
@@ -7,13 +7,9 @@ new Vue({
     strings: [],
     fonts: {},
     currentFontCode: null,
-    keyboard: 'th',
-    thEnMappings: {},
-    keyData: [],
-    keyMaps: {},
-    keyOthers: [],
-    combine: null,
-    keys: [],
+    keys: {},
+    rshift: 0,
+    maxRshift: 0,
     fontSize: '2em',
     textAlign: 'left'
   },
@@ -24,22 +20,8 @@ new Vue({
       }
       return this.fonts[this.currentFontCode].className;
     },
-    keySet: function() {
-      let result = '';
-      if (this.combine) {
-        result += this.combine + '(';
-      }
-      result += this.keys.join(',');
-      if (this.combine) {
-        result += ')';
-      }
-      return result;
-    },
     specialBtnClass: function() {
-      return this.combine === 'a' ? {'btn-dark': true} : {'btn-secondary': true};
-    },
-    sakotBtnClass: function() {
-      return this.combine === 's' ? {'btn-danger': true} : {'btn-primary': true};
+      return this.rshift ? {'btn-danger': true} : {'btn-secondary': true};
     },
     textAreaStyle: function() {
       return {
@@ -67,13 +49,10 @@ new Vue({
         document.body.classList.remove('dark');
       }
     },
-    loadKeys: async function(url = './data/keys.csv') {
+    loadKeys: async function(url = './data/keys.csv?v=' + VERSION) {
       const data = (await axios.get(url)).data.split('\n').map(row => row.split(';'));
       const headers = data.splice(0, 1)[0];
 
-      this.keyData = [];
-      this.keyMaps = {};
-      this.keyOthers = [];
       for (let i = 0; i < data.length; i++) {
         if (data[i].length != headers.length) {
           continue;
@@ -82,13 +61,24 @@ new Vue({
         for (let j = 0; j < headers.length; j++) {
           row[headers[j]] = data[i][j];
         }
-        row['UNICODE'] = row['UNICODE'].replace(/\\u([a-fA-F0-9]*)/g, (g, m1) => String.fromCharCode(parseInt(m1, 16)));
-        this.keyData.push(row);
-        if (row['KEYCODES']) {
-          this.keyMaps[row['KEYCODES']] = row['UNICODE'];
-        } else {
-          this.keyOthers.push(row['UNICODE']);
+
+        const char = row['UNICODE'].replace(/\\u([a-fA-F0-9]*)/g, (g, m1) => String.fromCharCode(parseInt(m1, 16)));
+        const name = row['NAME'];
+        const description = row['DESCRIPTION'];
+        const thai = row['THAI'];
+        const key = row['KEY'];
+        const rshift = row['RSHIFT'];
+
+        if (!this.keys[key]) {
+          this.keys[key] = {};
         }
+        this.keys[key][rshift] = {
+          char: char,
+          name: name,
+          description: description,
+          thai: thai,
+        };
+        this.maxRshift = Math.max(this.maxRshift, rshift);
       }
     },
     getTextareaElement: function() {
@@ -102,66 +92,26 @@ new Vue({
         + char + $textarea.value.substr(selectionEnd, $textarea.value.length);
       $textarea.setSelectionRange(selectionStart + char.length, selectionStart + char.length);
     },
-    addKeyset: function(keySet, e) {
-      if (this.keyMaps[keySet]) {
-        e.preventDefault();
-        this.addToTextarea(this.keyMaps[keySet]);
-        return true;
-      }
-      return false;
-    },
-    getCombiningKey: function(e) {
-      if ((e.keyCode === 18 || e.keyCode === 93) && e.location === 2) { // right alt or cmd
-        return 'a';
-      } else if (e.keyCode === 69) { // e
-        return 'e';
-      }
-      return null;
-    },
-    toKeyCode: function(code) {
-      if (this.keyboard === 'th') {
-        return this.thEnMappings[code] ? this.thEnMappings[code] : code;
-      } else if (this.keyboard === 'en') {
-        return code;
-      }
-    },
-    clearKey: function() {
-      this.combine = null;
-      this.keys = [];
-    },
-    keydown: function(e) {
-      if (this.combine) {
-        e.preventDefault();
-        const key = this.toKeyCode(e.key.charCodeAt(0));
-        if (key) {
-          this.keys.push(key);
-        }
-      } else {
-        this.combine = this.getCombiningKey(e);
-      }
-    },
+    keydown: function() {},
     keyup: function(e) {
-      if (this.combine && ((this.combine === this.getCombiningKey(e)) || (this.combine === 's'))) {
-        if (this.combine === 's') {
-          const key = this.toKeyCode(e.key.charCodeAt(0));
-          if (key) {
-            this.keys.push(key);
-            this.combine = 'a';
-          }
-        }
-
-        if (!this.addKeyset(this.keySet, e) && this.combine === 'a' && this.keys.length) {
-          const charCode = this.keys.pop();
-          if (this.keyMaps[charCode]) {
-            this.addToTextarea('\u1A60' + this.keyMaps[charCode]);
-          }
-        }
-        this.clearKey();
+      if (e.key === 'Shift' && (e.location === 0 || e.location === 2)) {
+        e.preventDefault();
+        this.special();
+      } else if ((e.key === 'Alt' || e.key === 'AltGraph') && e.location === 2) {
+        e.preventDefault();
+        this.sakot();
       }
     },
     keypress: function(e) {
-      if (!this.combine) {
-        this.addKeyset(this.toKeyCode(e.which), e);
+      const eventKey = e.key;
+      if (this.keys[eventKey]) {
+        const key = this.keys[eventKey][this.rshift] ? this.keys[eventKey][this.rshift] : this.keys[eventKey][0];
+        if (!key) {
+          return;
+        }
+        e.preventDefault();
+        this.rshift = 0;
+        this.addToTextarea(key.char);
       }
     },
     copy: function() {
@@ -187,19 +137,12 @@ new Vue({
       tempTextarea.remove();
     },
     sakot: function() {
-      this.combine = 's';
-      this.getTextareaElement().focus();
+      this.addToTextarea(this.keys.RA[0].char);
     },
     special: function() {
-      if (this.combine === 'a') {
-        this.keyup({
-          keyCode: 18,
-          location: 2,
-          preventDefault: () => {}
-        });
-      } else {
-        this.combine = 'a';
-      }
+      this.rshift = this.rshift >= this.maxRshift ? 0 : this.rshift + 1;
+    },
+    focus: function() {
       this.getTextareaElement().focus();
     },
     setFontSize: function(newFontSize) {
@@ -245,23 +188,12 @@ new Vue({
     
     this.loadKeys();
 
-    const thEnMappings = (await axios.get('./data/th-en-layout.csv?v=' + VERSION)).data.split('\n')
-      .filter(x => x).map(x => x.split(';').map(y => y.trim()))
-      .filter(x => x.length === 2);
-    for (let i = 0; i < thEnMappings.length; i++) {
-      this.thEnMappings[thEnMappings[i][0]] = thEnMappings[i][1];
-    }
-
     const localStorageLanguage = localStorage.getItem('language');
-    const localStorageKeyboard = localStorage.getItem('keyboard');
     const localStorageFont = localStorage.getItem('font');
     const localStorageFontSize = localStorage.getItem('fontSize');
     const localStorageTextAlign = localStorage.getItem('textAlign');
     if (localStorageLanguage) {
       this.setLanguage(localStorageLanguage);
-    }
-    if (localStorageKeyboard) {
-      this.keyboard = localStorageKeyboard;
     }
     if (localStorageFont) {
       this.currentFontCode = localStorageFont;
