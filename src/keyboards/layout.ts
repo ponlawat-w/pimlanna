e242e280e1bd791a67cd683581308fa925ef5ab3
Ponlawat W <ponlawat_w@cmu.ac.th>
2023-03-15 20:40:38 +0700

    Added keyboard library

---
+++ src/keyboards/layout.ts
@@ -0,0 +1,64 @@
+type LayoutButton = {
+  key: string,
+  keyShifted: string,
+  latin: string
+};
+
+type LayoutRow = LayoutButton[];
+
+export type InputLayout = LayoutRow[];
+
+export const ThaiKedmanee: InputLayout = [
+  [
+    {key: '_', keyShifted: '%', latin: '`'},
+    {key: 'ๅ', keyShifted: '+', latin: '1'},
+    {key: '/', keyShifted: '๑', latin: '2'},
+    {key: '-', keyShifted: '๒', latin: '3'},
+    {key: 'ภ', keyShifted: '๓', latin: '4'},
+    {key: 'ถ', keyShifted: '๔', latin: '5'},
+    {key: 'ุ', keyShifted: 'ู', latin: '6'},
+    {key: 'ึ', keyShifted: '฿', latin: '7'},
+    {key: 'ค', keyShifted: '๕', latin: '8'},
+    {key: 'ต', keyShifted: '๖', latin: '9'},
+    {key: 'จ', keyShifted: '๗', latin: '0'},
+    {key: 'ข', keyShifted: '๘', latin: '-'},
+    {key: 'ช', keyShifted: '๙', latin: '='},
+  ], [
+    {key: 'ๆ', keyShifted: '๐', latin: 'q'},
+    {key: 'ไ', keyShifted: '"', latin: 'w'},
+    {key: 'ำ', keyShifted: 'ฎ', latin: 'e'},
+    {key: 'พ', keyShifted: 'ฑ', latin: 'r'},
+    {key: 'ะ', keyShifted: 'ธ', latin: 't'},
+    {key: 'ั', keyShifted: 'ํ', latin: 'y'},
+    {key: 'ี', keyShifted: '๊', latin: 'u'},
+    {key: 'ร', keyShifted: 'ณ', latin: 'i'},
+    {key: 'น', keyShifted: 'ฯ', latin: 'o'},
+    {key: 'ย', keyShifted: 'ญ', latin: 'p'},
+    {key: 'บ', keyShifted: 'ฐ', latin: '['},
+    {key: 'ล', keyShifted: ',', latin: ']'},
+    {key: 'ฃ', keyShifted: 'ฅ', latin: '\\'}
+  ], [
+    {key: 'ฟ', keyShifted: 'ฤ', latin: 'a'},
+    {key: 'ห', keyShifted: 'ฆ', latin: 's'},
+    {key: 'ก', keyShifted: 'ฏ', latin: 'd'},
+    {key: 'ด', keyShifted: 'โ', latin: 'f'},
+    {key: 'เ', keyShifted: 'ฌ', latin: 'g'},
+    {key: '้', keyShifted: '็', latin: 'h'},
+    {key: '่', keyShifted: '๋', latin: 'j'},
+    {key: 'า', keyShifted: 'ษ', latin: 'k'},
+    {key: 'ส', keyShifted: 'ศ', latin: 'l'},
+    {key: 'ว', keyShifted: 'ซ', latin: ';'},
+    {key: 'ง', keyShifted: '.', latin: '\''}
+  ], [
+    {key: 'ผ', keyShifted: '(', latin: 'z'},
+    {key: 'ป', keyShifted: ')', latin: 'x'},
+    {key: 'แ', keyShifted: 'ฉ', latin: 'c'},
+    {key: 'อ', keyShifted: 'ฮ', latin: 'v'},
+    {key: 'ิ', keyShifted: 'ฺ', latin: 'b'},
+    {key: 'ื', keyShifted: '์', latin: 'n'},
+    {key: 'ท', keyShifted: '?', latin: 'm'},
+    {key: 'ม', keyShifted: 'ฒ', latin: ','},
+    {key: 'ใ', keyShifted: 'ฬ', latin: '.'},
+    {key: 'ฝ', keyShifted: 'ฦ', latin: '/'},
+  ]
+];
---
+++ src/keyboards/template.ts
@@ -0,0 +1,63 @@
+import type { InputLayout } from './layout';
+import type { KeyMapping } from './mapping';
+
+export class KeyboardButton {
+  public readonly key: string;
+  public readonly keyShifted: string;
+  public readonly latin: string;
+  public readonly taitham: string[];
+  public readonly taithamShifted: string[];
+
+  private constructor (
+    key: string,
+    keyShifted: string,
+    latin: string,
+    taitham: string[],
+    taithamShifted: string[]
+  ) {
+    this.key = key;
+    this.keyShifted = keyShifted;
+    this.latin = this.key !== latin && this.keyShifted !== latin ? latin : '';
+    this.taitham = taitham;
+    this.taithamShifted = taithamShifted;
+  }
+
+  public getCurrentTaitham(rShiftCount: number = 0): string {
+    return this.taitham[rShiftCount] ?? '';
+  }
+
+  public getCurrentShiftedTaitham(rShiftCount: number = 0): string {
+    return this.taithamShifted[rShiftCount] ?? '';
+  }
+
+  public getCurrent(lShift: boolean, rShiftCount: number = 0): string {
+    return (lShift ? this.taithamShifted : this.taitham)[rShiftCount] ?? '';
+  }
+
+  private static getTaithamFromKeyMappings(keyMappings: KeyMapping[]): string[] {
+    return Object.assign([], keyMappings.reduce<{[index: number]: string}>((obj, curr) => ({...obj, [curr.rShiftCount ?? 0]: curr.taitham}), {}));
+  }
+
+  public static getKeyboardLayout(keyMappings: KeyMapping[], inputLayout: InputLayout): KeyboardLayout {
+    const rows: KeyboardLayout = [];
+    for (const layoutRow of inputLayout) {
+      const row: KeyboardRow = [];
+      for (const layoutButton of layoutRow) {
+        const { key, keyShifted, latin } = layoutButton;
+        const buttonKeyMappings = keyMappings.filter(x => x.key === key);
+        const shiftedButtonKeyMappings = keyMappings.filter(x => x.key === keyShifted);
+        const button = new KeyboardButton(
+          key, keyShifted, latin,
+          KeyboardButton.getTaithamFromKeyMappings(buttonKeyMappings),
+          KeyboardButton.getTaithamFromKeyMappings(shiftedButtonKeyMappings)
+        );
+        row.push(button);
+      }
+      rows.push(row);
+    }
+    return rows;
+  }
+};
+
+export type KeyboardRow = KeyboardButton[];
+export type KeyboardLayout = KeyboardRow[];
